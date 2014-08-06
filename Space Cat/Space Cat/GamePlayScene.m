3677da67426c4911b4d9dde4b1c3f129aafeabf5
kevinegstorf <kevinegstorf@kevins-MacBook-Pro.local>
2014-08-06 17:32:23 +0200

    added game time to game scene

--- Space Cat/Space Cat/GamePlayScene.m
+++ Space Cat/Space Cat/GamePlayScene.m
@@ -18,8 +18,18 @@
         background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
         [self addChild:background];
         
+        
+        SKSpriteNode *machine = [SKSpriteNode spriteNodeWithImageNamed:@"machine_1"];
+        machine.position = CGPointMake(CGRectGetMidX(self.frame), 12);
+        machine.anchorPoint = CGPointMake(0.5, 0);
+        [self addChild:machine];
+        
     }
     return self;
+}
+
+-(void) update:(NSTimeInterval)currentTime {
+    NSLog(@"%f", fmod(currentTime, 60));
 }
 
 @end
